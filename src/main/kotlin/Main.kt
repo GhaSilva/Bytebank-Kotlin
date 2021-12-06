e9114af76d254395a26d4d7fee29b2a97d8d450d
Ghabriel Winicius <ghabriel-silva@outlook.com>
2021-12-06 14:21:00 -0300

    POO

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,51 @@
 fun main() {
     println("Bem vindo ao bytebank")
-    val titular: String = "Alex"
-    val numeroConta: Int = 1000
-    var saldo: Double = 0.0
-//    saldo = 100 + 2.0
+    val contaAlex = Conta()
+    contaAlex.titular = "Alex"
+    contaAlex.numero = 1000
+    contaAlex.saldo = 200.0
+    println(contaAlex.titular)
+    println(contaAlex.numero)
+    println(contaAlex.saldo)
 
-    println("Titular: $titular")
-    println("Número da conta: $numeroConta")
-    println("Saldo: $saldo")
+
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1001
+    contaFran.saldo = 300.0
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
+    }
+    class Conta {
+        var titular = "";
+        var numero = 0;
+        var saldo = 0.0;
+
+    }
+
+fun testaCondicoes(saldo: Double) {
 
     when {
         saldo > 0.0 -> println("Conta é positiva")
         saldo == 0.0 -> println("Conta é neutra")
         else -> println("Conta é negativa")
+    }
+}
+fun testaLaco(){
+    var i = 0
+
+    while (i < 5) {
+        val titular: String = "Alex $i"
+        val numeroConta: Int = 1000 + i
+        var saldo = i + 10.0
+        saldo = -100 + 2.0
+
+        println("Titular: $titular")
+        println("Número da conta: $numeroConta")
+        println("Saldo: $saldo")
+        println()
+        i++
 
     }
 }
